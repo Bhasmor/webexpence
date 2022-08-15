6b191042b025026acf1bb20d22f94549fde21f8b
Ahmet Tutsak <ahmettutsak@hotmail.com>
2022-08-15 20:30:53 +0300

    deleted daily graph

--- src/Components/data/Data.js
+++ src/Components/data/Data.js
@@ -147,36 +147,7 @@ function Data() {
     ],
   };
 
-  const datam = {
-    labels:
-      checkIfSameDate(incoexpence).length !== undefined
-        ? checkIfSameDate(incoexpence).map((item) => item.date)
-        : "empty",
-    datasets: [
-      {
-        label: "Daily Balance",
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: "rgba(75,192,192,0.4)",
-        borderColor: "rgba(75,192,192,1)",
-        borderJoinStyle: "miter",
-        pointBorderColor: "rgba(75,192,192,1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 5,
-        pointHitRadius: 10,
-        data:
-          checkIfSameDate(incoexpence).length !== undefined
-            ? checkIfSameDate(incoexpence).map((item) => item.amount)
-            : "empty",
-      },
-    ],
-  };
-
+  
   const categoryData = {
     labels:
       sameCategory(incoexpence).length !== undefined
@@ -191,6 +162,7 @@ function Data() {
           "rgba(75, 192, 192, 0.2)",
           "rgba(153, 102, 255, 0.2)",
           "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
         ],
         data:
           sameCategory(incoexpence).length !== undefined
@@ -204,9 +176,6 @@ function Data() {
     <div className="data-container">
       <div className="all-data">
         <Line data={pointData} />
-      </div>
-      <div className="all-data">
-        <Line data={datam} />
       </div>
       <div className="doug-data">
         <h2>Expence Details</h2>
